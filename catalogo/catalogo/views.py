a609e834d9af744bf9596a8d48217473b69f1d60
AlexanderVilla47 <alexandervilla.cuenta@gmail.com>
2022-12-20 15:53:32 -0300

    desarrollo apps

--- catalogo/catalogo/views.py
+++ catalogo/catalogo/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return render(request, 'index.html')
+    template_name= "index.html"
+    contexto= {
+        'nombre': "Alex"
+    }
+    return render(request, template_name, contexto)
