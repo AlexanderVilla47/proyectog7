222f1b745dae42c3e862c049734bd6407593bd2f
AlexanderVilla47 <alexandervilla.cuenta@gmail.com>
2022-12-20 13:16:36 -0300

    inicio no funciona

--- catalogo/catalogo/views.py
+++ catalogo/catalogo/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def inicio(request):
-    template_name = "index.html"
-    contexto = {
-    }
-    return render (request, template_name,contexto)
+    return render(request, 'index.html')
