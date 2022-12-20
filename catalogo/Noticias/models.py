6017d170fcf60be17bf47f0ab14b80020b4daaeb
AlexanderVilla47 <alexandervilla.cuenta@gmail.com>
2022-12-20 20:25:00 -0300

    sql noticias

--- catalogo/Noticias/models.py
+++ catalogo/Noticias/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Noticias(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=800)
+    imagen = models.ImageField ( upload_to = None , height_field = None , width_field = None , max_length = None ,)
+    fecha = models.CharField ( max_length = 10)
