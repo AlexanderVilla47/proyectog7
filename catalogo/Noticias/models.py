729153ef15ad151db12ceb49e8774e6451f6844d
AlexanderVilla47 <alexandervilla.cuenta@gmail.com>
2022-12-20 22:08:34 -0300

    inicio carrusel

--- catalogo/Noticias/models.py
+++ catalogo/Noticias/models.py
@@ -5,3 +5,6 @@ class Noticias(models.Model):
     descripcion = models.CharField(max_length=800)
     imagen = models.ImageField ( upload_to = None , height_field = None , width_field = None , max_length = None ,)
     fecha = models.CharField ( max_length = 10)
+    
+    def __str__(self):
+        return self.titulo
--- catalogo/catalogo/views.py
+++ catalogo/catalogo/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from Noticias.models import Noticias
+
 def inicio(request):
     template_name= "index.html"
+    
+    #n = Noticias.objects.create(
+    #    titulo= "Salvamos un gato",
+    #    descripcion= "Hoy encontramos un gato casi muerto y lo salvamos :)",
+    #    fecha= "10/11/1000"
+    #)
+    
+    Noticia= Noticias.objects.all()
+    
     contexto= {
-        'nombre': "Alex"
+        'Not': Noticias
     }
     return render(request, template_name, contexto)
