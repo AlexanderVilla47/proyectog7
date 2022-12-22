c4746a844aad7deb64b0b1c97cd50afa42dd1dd7
AlexanderVilla47 <alexandervilla.cuenta@gmail.com>
2022-12-22 12:31:32 -0300

    html

--- ONG/Noticias/models.py
+++ ONG/Noticias/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Noticias(models.Model):
     titulo = models.CharField(max_length=200)
-    descripcion = models.CharField(max_length=800)
+    descripcion = models.CharField(max_length=8000)
     fecha = models.CharField ( max_length = 10)
     
     activo= models.BooleanField(default=True)
--- ONG/Usuarios/forms.py
+++ ONG/Usuarios/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
 
 class UsuarioForm(UserCreationForm):
+    username= forms.CharField(widget= forms.TextInput(attrs={"class": "form-control"}))
+    first_name= forms.CharField(widget= forms.TextInput(attrs={"class": "form-control"}))
+    last_name= forms.CharField(widget= forms.TextInput(attrs={"class": "form-control"}))
+    email= forms.CharField(widget= forms.TextInput(attrs={"class": "form-control"}))
     class Meta:
         model= Usuario
         fields=["username", "first_name", "last_name", "email",]
